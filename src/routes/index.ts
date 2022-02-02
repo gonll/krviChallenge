0b0139e3451df99a8b8d5359f16349e7583a4107
Gonzalo Herrera <gonzalohll@gmail.com>
2022-02-02 11:42:12 -0300

    Implement routes

---
+++ src/routes/index.ts
@@ -0,0 +1,7 @@
+import express from 'express';
+import firstEndpointController from '../controllers/firstEndpointController';
+
+const router = express.Router();
+
+router.get('/firstEndpoint', firstEndpointController);
+/* router.get('/secondEndpoint', secondEndpointController); */
