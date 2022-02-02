3e7a6c62ebd0b94d398212bcbad66e6147b24e80
Gonzalo Herrera <gonzalohll@gmail.com>
2022-02-02 11:41:20 -0300

    Move index to src folder

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import routes from './src/routes';
 
 const app = express();
 const port = 3000;
@@ -6,3 +7,4 @@ const port = 3000;
 app.get('/', (req, res) => res.send("It's alive!"));
 
 app.listen( port,() => console.log(`Server running on port ${port}!`) );
+app.use('/', routes)
