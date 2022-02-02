1455465fbd278fe2b5d8cc7d874fa0607ab65c94
Gonzalo Herrera <gonzalohll@gmail.com>
2022-02-02 15:48:51 -0300

    Implement secondEndpointController

--- src/controllers/secondEndpointController.ts
+++ src/controllers/secondEndpointController.ts
@@ -1,21 +1,23 @@
 import { Request, Response } from "express";
+import { ICar } from "../common/Types";
 import getUsedCarsFromApi from "../services/getUsedCarsFromApi";
 
 const secondEndpointController = async (req: Request, res: Response) => {
-    if(!req.query.site || typeof req.query.site !== 'string') {
+    if (!req.query.site || typeof req.query.site !== 'string' || !req.query.ids) {
         /* 
             Esta verificación en realidad se hace en el middleware.
             La repito acá solo para demostrar la buena práctica de primero validar el negativo del requisito y retornar error.
-        */ 
-        res.status(400).send('El parámetro site es obligatorio en el request y debe ser un string.');
-    }else{
+        */
+        res.status(400).send('Los parámetros site y ids son obligatorios en el request y deben ser de tipo string.');
+    } else {
         const site: string = req.query.site.toString(); //El parseo a string es opcional y solo evita el clash de tipos de ts.
+        const ids: string[] = req.query.ids.toString().split(',');
         try {
-            const response = await getUsedCarsFromApi(site);
-            console.log('response: ', response[0]);
+            let response: ICar[] = await getUsedCarsFromApi(site);
+            response = response.filter((car: ICar) => car.id && ids.includes((car.id.toString())));
             res.status(200).send(response);
         } catch (error) {
-            console.log('Error secondEndpointController',error);  //Acá en realidad, reportaríamos el error en vez de loguearlo por consola.
+            console.log('Error secondEndpointController', error);  //Acá en realidad, reportaríamos el error en vez de loguearlo por consola.
             res.status(500).send('Ocurrió un error al procesar el pedido.');
         }
     }
