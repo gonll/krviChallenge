564837176defe0eac011bd199291dd9dae60f50a
Gonzalo Herrera <gonzalohll@gmail.com>
2022-02-02 12:13:36 -0300

    Change index from js to ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express from 'express';
-import routes from './src/routes';
+import routes from './routes';
 
 const app = express();
 const port = 3000;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.get('/', (req, res) => res.send("It's alive!"));
 
