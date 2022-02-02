2d2d675fcbbe12881a6a84e8a6a1ff02278b8281
Gonzalo Herrera <gonzalohll@gmail.com>
2022-02-02 17:52:54 -0300

    Create a common/envieronment

---
+++ src/common/Environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    TOKEN_API: process.env.TOKEN_API || '826b5e6c-49cc-4362-a0e3-658dc20fdbf2',
+    USED_CARS_API: process.env.USED_CARS_API || 'https://used-cars-api.development.karvi.com.ar/cars/challenge'
+}
