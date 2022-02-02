a98f68b4346ecaea397e090cc3fae3a61e4cd133
Gonzalo Herrera <gonzalohll@gmail.com>
2022-02-02 11:42:57 -0300

    Implement service's type

---
+++ src/services/Types.ts
@@ -0,0 +1,18 @@
+export interface IEnpointResponse {
+    id?: number;
+    city?: string;
+    state?: string;
+    year?: string;
+    brand?: string;
+    model?: string;
+    version?: string;
+    price?: number;
+    mileage?: number;
+    image?: string;
+    certificate?: boolean;
+    promoted?: boolean
+}
+/* 
+    Lo ideal sería que estos parámetros no sean opcionales, pero para eso necesitaría tener el modelo de la entidad.
+    En este caso, solamente infiero que el modelo de la entidad es el mismo que el de la respuesta.
+*/
