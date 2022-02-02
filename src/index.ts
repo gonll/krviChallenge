73aad09d1a519c5da8421f1fa6c61d050a095879
Gonzalo Herrera <gonzalohll@gmail.com>
2022-02-02 19:48:52 -0300

    Modify port to 0 when testing

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,10 @@ app.use(express.json());
 
 app.get('/', (req, res) => res.status(200).send("It's alive!"));
 
-app.listen( port,() => console.log(`Server running on port ${port}!`) );
+if(process.env.NODE_ENV !== 'test') { 
+    //En caso de ser un test, el puerto se asignará a 0 (minimo disponible)
+    app.listen( port,() => console.log(`Server running on port ${port}!`) );
+}
 app.use('/', routes);
 
 export default app;
