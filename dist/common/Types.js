8dedcf4d79bbf8ca442fda9df363437077ea4bba
Gonzalo Herrera <gonzalohll@gmail.com>
2022-02-03 08:18:37 -0300

    Changed dist common types to lower case because Heroku

--- dist/common/Types.js
+++ dist/common/Types.js
@@ -3,4 +3,4 @@ Object.defineProperty(exports, "__esModule", { value: true });
 /*
     Lo ideal sería que estos parámetros no sean opcionales, pero para eso necesitaría tener el modelo de la entidad.
     En este caso, solamente infiero que el modelo de la entidad es el mismo que el de la respuesta.
-*/ 
+*/
