8dd7bd4858a37b4e2d8eebd78786011587a05097
Gonzalo Herrera <gonzalohll@gmail.com>
2022-02-07 10:32:49 -0300

    Update routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 import { checkParams } from '../middlewares/checkParams';
-import firstEndpointController from '../controllers/firstEndpointController';
-import secondEndpointController from '../controllers/secondEndpointController';
+import carsBySiteController from '../controllers/carsBySiteController';
+import carsBySiteIdsController from '../controllers/carsBySiteIdsController';
 
 const router = express.Router();
 
-router.get('/firstEndpoint', checkParams(), firstEndpointController);
-router.get('/secondEndpoint', checkParams(), secondEndpointController);
+router.get('/carsBySite', checkParams(), carsBySiteController);
+router.get('/carsBySiteIds', checkParams(), carsBySiteIdsController);
 
 export default router;
