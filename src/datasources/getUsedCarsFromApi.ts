cbb6d70b36193e68199110515c780a11e0e70ccb
Gonzalo Herrera <gonzalohll@gmail.com>
2022-02-07 10:33:56 -0300

    Format datasource

--- src/datasources/getUsedCarsFromApi.ts
+++ src/datasources/getUsedCarsFromApi.ts
@@ -5,7 +5,7 @@ const axios = require('axios').default;
 
 const getUsedCarsFromApi = async (site: string): Promise<any> => {
     const endPoint: string = process.env.USED_CARS_API || 'https://used-cars-api.development.karvi.com.ar/cars/challenge';
-    const headers: {'api-key': string} = { 'api-key': process.env.TOKEN_API || '826b5e6c-49cc-4362-a0e3-658dc20fdbf2' }; //Estos valores probablemente los deberíamos configurar en el secret del pod si esto no fuera un challenge.
+    const headers: { 'api-key': string } = { 'api-key': process.env.TOKEN_API || '826b5e6c-49cc-4362-a0e3-658dc20fdbf2' }; //Estos valores probablemente los deberíamos configurar en el secret del pod si esto no fuera un challenge.
 
     const axiosConfig = {
         headers: headers,
@@ -13,7 +13,7 @@ const getUsedCarsFromApi = async (site: string): Promise<any> => {
             site: site
         }
     }
-    
+
     let data: ICar[] = [];
     try {
         const response: AxiosResponse = await axios.get(`${endPoint}`, axiosConfig);
